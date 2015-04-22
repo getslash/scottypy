e6b8ede23c932d14817d5cf08278679b5e845ab0
Roey Darwish Dror <roeyd@infinidat.com>
2015-04-22 12:58:52 +0300

    Frontend app configure logging

--- scotty/frontend.py
+++ scotty/frontend.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import logging
 import sys
 from . import beam_up
 
@@ -9,11 +10,13 @@ def main():
         print('  scotty - Optional URL for scotty. Defaults to http://scotty')
         return 1
 
+    logging.basicConfig(format='%(name)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+
     kwargs = {
         'directory': sys.argv[1]
     }
     if len(sys.argv) > 2:
         kwargs['scotty_url'] = sys.argv[2]
 
-    print('Beaming up {0}'.format(sys.argv[1]))
+    logging.info('Beaming up {0}'.format(sys.argv[1]))
     beam_up(**kwargs)
