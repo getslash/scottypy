8c8d87414814517a62c987135bc992effe7bf92e
Roey Darwish Dror <roeyd@infinidat.com>
2015-05-12 14:33:46 +0300

    Add compression checking

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,5 +1,17 @@
 import os
+import gzip
+from contextlib import closing
 from slash import parametrize
+
+
+_LOG = """I can see what you see not
+Vision milky, then eyes rot.
+When you turn, they will be gone,
+Whispering their hidden song.
+Then you see what cannot be
+Shadows move where light should be.
+Out of darkness, out of mind,
+Cast down into the Halls of the Blind."""
 
 
 def _local_beam(scotty):
@@ -23,3 +35,36 @@ def test_sanity(scotty, beam_function):
     scotty.add_tag(beam_id, 'test')
     scotty.remove_tag(beam_id, 'test')
     scotty.get_beam(beam_id)
+
+
+def test_compressed_file(scotty, tempdir):
+    log_path = os.path.join(tempdir, "something.log")
+    with open(log_path, "w") as log_file:
+        log_file.write(_LOG)
+
+    txt_path = os.path.join(tempdir, "something.txt")
+    with open(txt_path, "w") as txt_file:
+        txt_file.write(_LOG)
+
+    beam_id = scotty.beam_up(tempdir)
+    beam = scotty.get_beam(beam_id)
+    assert len(beam.files) == 2
+
+    found_compressed = False
+    found_uncompressed = False
+
+    for beamed_file in beam.files:
+        full_path = os.path.join("/var/scotty", beamed_file.storage_name)
+        if full_path.endswith(".gz"):
+            found_compressed = True
+            with closing(gzip.GzipFile(full_path, "r")) as f:
+                content = f.read().decode("ASCII")
+        else:
+            found_uncompressed = True
+            with open(full_path, "r") as f:
+                content = f.read()
+
+        assert content == _LOG
+
+    assert found_uncompressed
+    assert found_compressed
