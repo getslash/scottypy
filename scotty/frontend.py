e8444b73382f94b717174d8bd5945a5c26c45720
Roey Darwish Dror <roeyd@infinidat.com>
2015-05-17 11:51:09 +0300

    Don't use a default scotty in the beaming app

--- scotty/frontend.py
+++ scotty/frontend.py
@@ -5,17 +5,14 @@ from . import Scotty
 
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: beamup [directory] <scotty>')
-        print('  scotty - Optional URL for scotty. Defaults to http://scotty')
+    if len(sys.argv) < 3:
+        print('Usage: beamup [directory] [scotty]')
+        print('  scotty - URL for scotty')
         return 1
 
     logging.basicConfig(format='%(name)s:%(levelname)s:%(message)s', level=logging.DEBUG)
 
-    if len(sys.argv) > 2:
-        scotty = Scotty(sys.argv[2])
-    else:
-        scotty = Scotty()
+    scotty = Scotty(sys.argv[2])
 
     logging.info('Beaming up %s', sys.argv[1])
     beam_id = scotty.beam_up(sys.argv[1])
