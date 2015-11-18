0ff6b15ebebd8891f4fbd631d3aa8300f18a8f64
Roey Darwish Dror <roeyd@infinidat.com>
2015-11-18 14:43:46 +0200

    INFRADEV-5710: Filter support in beamdown

--- scotty/beamdown.py
+++ scotty/beamdown.py
@@ -7,8 +7,9 @@ from . import Scotty, NotOverwriting
 @click.argument("beam_id")
 @click.option('--dest', default=None, help='Destination directory')
 @click.option('--url', default='http://scotty.infinidat.com', help='Base URL of Scotty')
+@click.option('-f', '--filter', default=None, help="Download only files that contain the given string in their name (case insensetive)")
 @click.option('--overwrite/--no-overwrite', default=False, help='Overwrite existing files on the disk')
-def main(beam_id, dest, url, overwrite):
+def main(beam_id, dest, url, overwrite, filter):
     scotty = Scotty(url)
 
     beam = scotty.get_beam(beam_id)
@@ -21,6 +22,10 @@ def main(beam_id, dest, url, overwrite):
     click.echo("Downloading beam {} to directory {}".format(beam_id, dest))
 
     for file_ in beam.iter_files():
+        if filter is not None and filter not in file_.file_name:
+            click.echo("Skipping {}".format(file_.file_name))
+            continue
+
         click.echo("Downloading {}".format(file_.file_name))
         try:
             file_.download(dest, overwrite=overwrite)
