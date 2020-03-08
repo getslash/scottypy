5ab56cf12212e264d55b6d86525e5ecd0d48310b
David Sternlicht <d1618033@gmail.com>
2020-03-08 08:49:34 +0200

    Add readme and bumpversion.sh

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Scotty Client Library  
+  
+## Notes For Developers  
+  
+### How to release a new version  
+  
+1. Update changelog in `README.md` file
+1. Run `./bumpversion.sh <version>`
+
+## ChangeLog
+
+### Unreleased
+
+* Support python3.8
+* Support Rust Combadge (v2)
---
+++ bumpversion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=$1
+echo "__version__ = \"$VERSION\"" > scottypy/__version__.py
+git add scottypy/__version__.py
+git commit -m "Bump version to $VERSION"
+git tag -a "$VERSION" -m "$VERSION" && git push --tags
+git flow release start "$VERSION"
+git push --set-upstream origin "release/$VERSION"
+git flow release finish "$VERSION"
+git push origin : --tags
