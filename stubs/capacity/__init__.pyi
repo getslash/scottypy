c5d063bcebc171fb30b0fad23b407547e5f9d2fa
David Sternlicht <dsternlicht@infinidat.com>
2020-04-07 15:57:02 +0300

    Add stubs

---
+++ stubs/capacity/__init__.pyi
@@ -0,0 +1,1 @@
+from .capacity import Capacity as Capacity, EB as EB, EiB as EiB, GB as GB, GiB as GiB, KB as KB, KiB as KiB, MB as MB, MiB as MiB, PB as PB, PiB as PiB, TB as TB, TiB as TiB, bit as bit, byte as byte, from_string as from_string
---
+++ stubs/pact/_compat.pyi
@@ -0,0 +1,12 @@
+from io import StringIO as StringIO
+from typing import Any, Optional
+
+PY2: Any
+zip: Any
+xrange = range
+iteritems: Any
+itervalues: Any
+integer_types: Any
+string_types: Any
+
+def reraise(tp: Any, value: Any, tb: Optional[Any] = ...) -> None: ...
